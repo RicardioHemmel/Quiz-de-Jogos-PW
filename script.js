763d2845aece9db3ac89d772b55404abdaf0a25b
Thiago Pereira Lins <thiagopls1@hotmail.com>
2022-05-28 14:54:59 -0300

    jorge

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-function WEEE(){
+var mainBox = document.getElementById('mainBox');
+var startButton = document.getElementById('startButton');
+var helpButton = document.getElementById('helpButton')
 
+function startQuiz(){
+    mainBox.style.height = '65vh';
+    startButton.style.display = 'none'
+    helpButton.style.display = 'none'
 }
