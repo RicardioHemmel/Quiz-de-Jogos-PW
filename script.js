efe20e2ec06936d6db3a90b74c3991a8d30c1178
Thiago Pereira Lins <thiagopls1@hotmail.com>
2022-05-28 15:25:09 -0300

    adicionando animacao

--- script.js
+++ script.js
@@ -3,7 +3,19 @@ var startButton = document.getElementById('startButton');
 var helpButton = document.getElementById('helpButton')
 
 function startQuiz(){
+    let id = null;
+    let h = mainBox.style.height;
+    clearInterval(id);
+    id = setInterval(frame, 5);
     mainBox.style.height = '65vh';
-    startButton.style.display = 'none'
-    helpButton.style.display = 'none'
+    startButton.style.display = 'none';
+    helpButton.style.display = 'none';
+    function frame(){
+        if (h == 65){
+            clearInterval(id)
+        } else {
+            h++
+            mainBox.style.height = `${h}vh`
+        }
+    }
 }
