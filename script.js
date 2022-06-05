7fd3a3d628ad7ec35c43594e79b5ae5a6ecaac73
Thiago Pereira Lins <thiagopls1@hotmail.com>
2022-06-05 15:26:31 -0300

    arrumando umas coisa

--- script.js
+++ script.js
@@ -86,21 +86,127 @@ function renderQuestion(){
         `;
         break;
         case 4:
+        divQuestoes.innerHTML = `
+        <div class="headingContainer">
+            <h1>${question} - Dentre estes jogos qual é o mais caro, incluído com as DLCs?</h1>
+        </div>
+        <div class="imageContainer">
+            <img src="./public/img/minecraft.gif">
+        </div>
+        <div class="buttonsContainer">
+            <button onClick="questionAnswered(false)" class="optionButton questionButton">Half-Life: Alyx</button>
+            <button onClick="questionAnswered(false)" class="optionButton questionButton">The Sims 4</button>
+            <button onClick="questionAnswered(false)" class="optionButton questionButton">The Last of Us Part II</button>
+            <button onClick="questionAnswered(true)" class="optionButton questionButton">Train Simulator</button>
+        </div>
+        `;
+        break;
+        case 5:
             divQuestoes.innerHTML = `
-            <div class="headingContainer">
-                <h1>${question} - THOAGP THOIAG THIAGPO THIAGO THIAGO THIAGO</h1>
-            </div>
-            <div class="imageContainer">
-                <img src="./public/img/minecraft.gif">
-            </div>
-            <div class="buttonsContainer">
-                <button onClick="questionAnswered(false)" class="optionButton questionButton">Activision</button>
-                <button onClick="questionAnswered(true)" class="optionButton questionButton">Mojang Studios</button>
-                <button onClick="questionAnswered(false)" class="optionButton questionButton">Epic Games</button>
-                <button onClick="questionAnswered(false)" class="optionButton questionButton">Supercell</button>
-            </div>
-            `;
-            break;
+        <div class="headingContainer">
+            <h1>${question} - Quem é este personagem?</h1>
+        </div>
+        <div class="imageContainer">
+            <img src="./public/img/minecraft.gif">
+        </div>
+        <div class="buttonsContainer">
+            <button onClick="questionAnswered(false)" class="optionButton questionButton">Raiden</button>
+            <button onClick="questionAnswered(false)" class="optionButton questionButton">Jacket</button>
+            <button onClick="questionAnswered(false)" class="optionButton questionButton">Dallas</button>
+            <button onClick="questionAnswered(true)" class="optionButton questionButton">Gordon Freeman</button>
+        </div>
+        `;
+        break;
+        case 6:
+            divQuestoes.innerHTML = `
+        <div class="headingContainer">
+            <h1>${question} - Em qual desses jogos o protagonista morre em sua continuação?</h1>
+        </div>
+        <div class="imageContainer">
+            <img src="./public/img/minecraft.gif">
+        </div>
+        <div class="buttonsContainer">
+            <button onClick="questionAnswered(false)" class="optionButton questionButton">Portal 2</button>
+            <button onClick="questionAnswered(false)" class="optionButton questionButton">PayDay 2</button>
+            <button onClick="questionAnswered(true)" class="optionButton questionButton">The Last of Us Part II</button>
+            <button onClick="questionAnswered(false)" class="optionButton questionButton">Half-Life 2</button>
+        </div>
+        `;
+        break;
+        case 7:
+            divQuestoes.innerHTML = `
+        <div class="headingContainer">
+            <h1>${question} - Qual desses jogos possui a menor nota de review?</h1>
+        </div>
+        <div class="imageContainer">
+            <img src="./public/img/minecraft.gif">
+        </div>
+        <div class="buttonsContainer">
+            <button onClick="questionAnswered(false)" class="optionButton questionButton">Fall Guys</button>
+            <button onClick="questionAnswered(false)" class="optionButton questionButton">Fallout 75</button>
+            <button onClick="questionAnswered(false)" class="optionButton questionButton">Metal Gear Survive</button>
+            <button onClick="questionAnswered(true)" class="optionButton questionButton">Grand Theft Auto: The Trilogy - The Definitive Edition</button>
+        </div>
+        `;
+        break;
+        case 8:
+        divQuestoes.innerHTML = `
+        <div class="headingContainer">
+            <h1>${question} - Em qual desses jogos Hack 'n Slash você consegue fazer combos com seus golpes pontuando do E ao S?</h1>
+        </div>
+        <div class="imageContainer">
+            <img src="./public/img/minecraft.gif">
+        </div>
+        <div class="buttonsContainer">
+            <button onClick="questionAnswered(false)" class="optionButton questionButton">Metal Gear Rising: Revengeance</button>
+            <button onClick="questionAnswered(true)" class="optionButton questionButton">Devil May Cry</button>
+            <button onClick="questionAnswered(false)" class="optionButton questionButton">Sekiro: Shadows Die Twice</button>
+            <button onClick="questionAnswered(false)" class="optionButton questionButton">Katana Zero</button>
+        </div>
+        `;
+        break;
+        case 9:
+            divQuestoes.innerHTML = `
+        <div class="headingContainer">
+            <h1>${question} - Depth é um jogo em que o jogador:</h1>
+        </div>
+        <div class="imageContainer">
+            <img src="./public/img/minecraft.gif">
+        </div>
+        <div class="buttonsContainer">
+            <button onClick="questionAnswered(false)" class="optionButton questionButton">Precisa descer em crateras para explorar.</button>
+            <button onClick="questionAnswered(true)" class="optionButton questionButton">Joga contra tubarões e com outros jogadores enquanto resgata ouro.</button>
+            <button onClick="questionAnswered(false)" class="optionButton questionButton">Cria uma base, explora o mundo e enfrenta uma diversidade de inimigos.</button>
+            <button onClick="questionAnswered(false)" class="optionButton questionButton">Tem que chegar o mais longe possível em uma masmorra.</button>
+        </div>
+        `;
+        break;
+        case 10:
+            divQuestoes.innerHTML = `
+        <div class="headingContainer">
+            <h1>${question} - Quem é o antagonista principal de Metal Gear Solid V: The Phantom Pain?</h1>
+        </div>
+        <div class="imageContainer">
+            <img src="./public/img/minecraft.gif">
+        </div>
+        <div class="buttonsContainer">
+            <button onClick="questionAnswered(false)" class="optionButton questionButton">Greyfox</button>
+            <button onClick="questionAnswered(true)" class="optionButton questionButton">Snake</button>
+            <button onClick="questionAnswered(false)" class="optionButton questionButton">Kazuhira Müller</button>
+            <button onClick="questionAnswered(false)" class="optionButton questionButton">Skull Face</button>
+        </div>
+        `;
+        break;
+        case 11:
+            divQuestoes.style.display = 'none';
+            divQuizFinalizado.style.display = "block";
+            divQuizFinalizado.innerHTML = `
+            <p>Sua pontuação: ${score}</p>
+            <br>
+            <button onclick="resetAndGoToMainMenu()" class="optionButton questionButton">Menu Principal</button>
+            `
+            ;
+        break;
     }
     console.log(`question: ${question}`);
     console.log(`score: ${score}`);
